e24b8e741df5a4bc355d8c5fadd9188ca2541c8c
kunal_cpp <kunaliesc@gmail.com>
2022-03-05 22:12:23 +0530

    Update Design LRU cache.cpp

--- Design LRU cache.cpp
+++ Design LRU cache.cpp
@@ -13,7 +13,7 @@ Explanation
 LRUCache lRUCache = new LRUCache(2);
 lRUCache.put(1, 1); // cache is {1=1}
 lRUCache.put(2, 2); // cache is {2=2, 1=1}
-lRUCache.get(1);    // return 1, cache is{1=1, 2=2}
+lRUCache.get(1);    // return (val of key 1), cache is{1=1, 2=2}
 lRUCache.put(3, 3); // cache is {3=3, 1=1}
 lRUCache.get(2);    // returns -1 (not found)
 lRUCache.put(4, 4); // cache is {4=4, 3=3}
